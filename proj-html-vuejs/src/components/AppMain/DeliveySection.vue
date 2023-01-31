b62841f301dbfa1dda7fa57caafbbb695d21fed8
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 00:37:38 +0100

    Background image added to delivery section

---
+++ proj-html-vuejs/src/components/AppMain/DeliveySection.vue
@@ -0,0 +1,17 @@
+<script></script>
+<template>
+    <section id="delivery">
+
+    </section>
+</template>
+
+<style lang="scss">
+@use '../../assets/scss/partials/mixins' as *;
+
+#delivery {
+    height: 530px;
+    width: auto;
+    background-image: url('../../assets/img/call-to-action-bg.jpg');
+    @include bg-image;
+}
+</style>
