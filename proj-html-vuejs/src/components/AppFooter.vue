9468bfd31f45195f17ec4f1a2dc09906c9d4d7d3
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-31 22:04:11 +0100

    imported in AppFooter both top and bottom footer

--- proj-html-vuejs/src/components/AppFooter.vue
+++ proj-html-vuejs/src/components/AppFooter.vue
@@ -1,1 +1,13 @@
-<template></template>
+<script>
+import TopFooter from './AppFooter/TopFooter.vue';
+import BottomFooter from './AppFooter/BottomFooter.vue';
+export default {
+    name: 'AppFooter',
+    components: { TopFooter, BottomFooter }
+}
+
+</script>
+<template>
+    <top-footer></top-footer>
+    <bottom-footer></bottom-footer>
+</template>
