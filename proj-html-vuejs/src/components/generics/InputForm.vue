bad1515f44f549966a9155f7b50f354c08884f16
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-31 22:41:42 +0100

    generic component 'InputForm'

---
+++ proj-html-vuejs/src/components/generics/InputForm.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'InputForm',
+    data() { return { value: '' } }
+}
+</script>
+<template>
+    <template>
+        <form @submit.prevent="$emit('submit')">
+            <input type="text" v-model.trim="value" @keyup="$emit('type', value)"
+                :placeholder="placeholder || 'Type here...'">
+            <button> {{ text || 'SEND' }}</button>
+        </form>
+    </template>
+</template>
