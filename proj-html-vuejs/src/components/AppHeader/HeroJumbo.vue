99f58ebe3dd76fdeea4331177d41a336a2555d49
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-31 16:33:44 +0100

    creation of HeroJumbo. BG image added

---
+++ proj-html-vuejs/src/components/AppHeader/HeroJumbo.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'HeroJumbo',
+}
+</script>
+<template>
+    <section id="hero-jumbo">
+
+    </section>
+</template>
+<style lang="scss">
+#hero-jumbo {
+    height: 750px;
+    width: auto;
+    background-image: url('../../assets/img/hero-header-bakery.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+</style>
