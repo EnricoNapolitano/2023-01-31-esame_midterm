8cc708867b51436d253355b50f0f6647603bb89f
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-31 16:01:47 +0100

    created top header, that contains: Logo and Nav

---
+++ proj-html-vuejs/src/components/AppHeader/TopHeader.vue
@@ -0,0 +1,16 @@
+<script>
+import { data } from '../../assets/data';
+import LogoCompany from '../generics/LogoCompany.vue';
+import NavMenu from '../generics/NavMenu.vue';
+export default {
+    name: 'TopHeader',
+    data() { return { data } },
+    components: { LogoCompany, NavMenu }
+}
+</script>
+<template>
+    <header>
+        <logo-company></logo-company>
+        <nav-menu :menu="data.header_nav"></nav-menu>
+    </header>
+</template>
