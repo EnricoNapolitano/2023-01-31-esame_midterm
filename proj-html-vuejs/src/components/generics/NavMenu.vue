39ab9a702c3ce582f34ccd52791b7f11acdad03f
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-31 15:59:15 +0100

    generic nav (it may be used for header and footer)

---
+++ proj-html-vuejs/src/components/generics/NavMenu.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: 'NavMenu',
+    props: { menu: Array }
+}
+</script>
+
+<template>
+    <ul>
+        <li v-for="item in menu"><a href="#">{{ item }}</a></li>
+    </ul>
+</template>
