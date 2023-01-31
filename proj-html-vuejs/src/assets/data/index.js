645cd72973f00c364e8b3d38f5c77a2e3362210b
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-31 23:35:11 +0100

    socials array added to index.js

--- proj-html-vuejs/src/assets/data/index.js
+++ proj-html-vuejs/src/assets/data/index.js
@@ -7,6 +7,9 @@ const data = {
     jumbo_title: 'The Perfect Fresh Bread',
     jumbo_paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium animi velit porro blanditiis amet ea praesentium corrupti nam consectetur aspernatur, nostrum in earum vitae, fugit repellat voluptates officia natus dignissimos.',
     jumbo_button: 'Explore our products',
+
+    //bottom footer: social icons
+    socials: ['fa-brands fa-instagram', 'fa-brands fa-twitter', 'fa-brands fa-facebook-f', 'fa-brands fa-pinterest-p'],
 }
 
 export { data }
