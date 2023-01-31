6476c5d4b43f843d1b5b6c496d3ec27c8ac79905
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-31 21:20:02 +0100

    style to ActionCall

--- proj-html-vuejs/src/components/generics/ActionCall.vue
+++ proj-html-vuejs/src/components/generics/ActionCall.vue
@@ -6,8 +6,36 @@ export default {
 </script>
 
 <template>
-    <h4>{{ label }}</h4>
-    <h2>{{ title }}</h2>
-    <p>{{ paragraph }}</p>
-    <button>{{ botton_label }}</button>
+    <div>
+        <h4>{{ label }}</h4>
+        <h2>{{ title }}</h2>
+        <p>{{ paragraph }}</p>
+        <button>{{ botton_label }}</button>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+div {
+    width: 530px;
+    padding: 30px;
+}
+
+h4,
+h2,
+p {
+    padding-bottom: 35px;
+}
+
+h2 {
+    font-size: 25px;
+}
+
+p {
+    line-height: 30px;
+}
+
+button {
+    padding: 20px 30px;
+    border-radius: 5px;
+}
+</style>
