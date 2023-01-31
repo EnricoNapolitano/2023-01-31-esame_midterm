f2a9cf01ec0d99a977b062a8f6a23a41551a1588
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-31 14:59:06 +0100

    macro structures: site's main parts

--- proj-html-vuejs/src/App.vue
+++ proj-html-vuejs/src/App.vue
@@ -1,2 +1,20 @@
-<script></script>
-<template></template>
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+export default {
+  name: 'App',
+  components: { AppHeader, AppMain, AppFooter }
+}
+</script>
+<template>
+  <app-header>
+    <!-- 2 components in it: top header with navbar; jumbotron -->
+  </app-header>
+  <app-main>
+    <!-- 9 components reflecting all the sections -->
+  </app-main>
+  <app-footer>
+    <!-- 2 components: top and bottom footer -->
+  </app-footer>
+</template>
