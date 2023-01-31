92eab97f2f6b1aa5207309ea49ac5e4e5777fd02
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-31 22:05:31 +0100

    TopFooter: added logo compani and nav

---
+++ proj-html-vuejs/src/components/AppFooter/TopFooter.vue
@@ -0,0 +1,21 @@
+<script>
+import { data } from '../../assets/data';
+import LogoCompany from '../generics/LogoCompany.vue';
+import NavMenu from '../generics/NavMenu.vue';
+export default {
+    name: 'TopFooter',
+    data() { return { data } },
+    components: { LogoCompany, NavMenu },
+    computed: {
+        //removing 'home' from nav array
+        footerMenu() { return this.data.nav.splice(1) }
+    }
+}
+</script>
+
+<template>
+    <footer class="ph-30">
+        <logo-company></logo-company>
+        <nav-menu :menu="footerMenu"></nav-menu>
+    </footer>
+</template>
