e1c92b309907658e3b2ecba1eaad152ea7ed665d
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-31 16:02:24 +0100

    Imported TopHeader in AppHeader

--- proj-html-vuejs/src/components/AppHeader.vue
+++ proj-html-vuejs/src/components/AppHeader.vue
@@ -1,1 +1,12 @@
-<template></template>
+<script>
+import TopHeader from './AppHeader/TopHeader.vue';
+export default {
+    name: 'AppHeader',
+    components: { TopHeader }
+}
+</script>
+<template>
+    <header>
+        <top-header></top-header>
+    </header>
+</template>
