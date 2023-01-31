858dca1748eafa455acc122870bac8de4f1b3dd0
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-31 21:55:25 +0100

    each nav element transformed in upper case

--- proj-html-vuejs/src/components/AppHeader/TopHeader.vue
+++ proj-html-vuejs/src/components/AppHeader/TopHeader.vue
@@ -5,12 +5,17 @@ import NavMenu from '../generics/NavMenu.vue';
 export default {
     name: 'TopHeader',
     data() { return { data } },
-    components: { LogoCompany, NavMenu }
+    components: { LogoCompany, NavMenu },
+    computed: {
+        upperCaseMenu() {
+            return this.data.nav.map(word => word.toUpperCase());
+        }
+    }
 }
 </script>
 <template>
     <header class="d-flex jc-between ai-center ph-30">
         <logo-company></logo-company>
-        <nav-menu :menu="data.nav"></nav-menu>
+        <nav-menu :menu="upperCaseMenu"></nav-menu>
     </header>
 </template>
