7d93c98e629e8b150a0b1181bf91a82269d7b65e
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-31 17:37:06 +0100

    data structure of call 2 action in HeroJumbo

--- proj-html-vuejs/src/assets/data/index.js
+++ proj-html-vuejs/src/assets/data/index.js
@@ -1,5 +1,12 @@
 const data = {
-    header_nav: ['HOME', 'SHOP', 'ABOUT', 'GALLERY', 'LOCATIONS', 'JOURNAL', 'CONTACT', 'MY ACCOUNT']
+    //top header
+    header_nav: ['HOME', 'SHOP', 'ABOUT', 'GALLERY', 'LOCATIONS', 'JOURNAL', 'CONTACT', 'MY ACCOUNT'],
+
+    //hero jumbo
+    jumbo_label: 'FRESH & TASTY BAKERY EVERY DAY',
+    jumbo_title: 'The Perfect Fresh Bread',
+    jumbo_paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium animi velit porro blanditiis amet ea praesentium corrupti nam consectetur aspernatur, nostrum in earum vitae, fugit repellat voluptates officia natus dignissimos.',
+    jumbo_button: 'Explore our products',
 }
 
 export { data }
--- proj-html-vuejs/src/components/AppHeader/HeroJumbo.vue
+++ proj-html-vuejs/src/components/AppHeader/HeroJumbo.vue
@@ -1,14 +1,17 @@
 <script>
+import { data } from '../../assets/data/index';
 import ActionCall from '../generics/ActionCall.vue';
 export default {
     name: 'HeroJumbo',
+    data() { return { data } },
     components: { ActionCall }
 }
 </script>
 
 <template>
     <section id="hero-jumbo">
-        <action-call></action-call>
+        <action-call :label="data.jumbo_label" :title="data.jumbo_title" :paragraph="data.jumbo_paragraph"
+            :botton_label="data.jumbo_button"></action-call>
     </section>
 </template>
 
