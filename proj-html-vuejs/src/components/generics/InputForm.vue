8a7a02ad368af248e2943c50e7e8e35e20bad560
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-31 22:48:49 +0100

    emits remove, props added

--- proj-html-vuejs/src/components/generics/InputForm.vue
+++ proj-html-vuejs/src/components/generics/InputForm.vue
@@ -1,14 +1,14 @@
 <script>
 export default {
     name: 'InputForm',
-    data() { return { value: '' } }
+    data() { return { value: '' } },
+    props: { placeholder: String, text: String }
 }
 </script>
 <template>
     <template>
-        <form @submit.prevent="$emit('submit')">
-            <input type="text" v-model.trim="value" @keyup="$emit('type', value)"
-                :placeholder="placeholder || 'Type here...'">
+        <form>
+            <input type="text" v-model.trim="value" :placeholder="placeholder || 'Type here...'">
             <button> {{ text || 'SEND' }}</button>
         </form>
     </template>
