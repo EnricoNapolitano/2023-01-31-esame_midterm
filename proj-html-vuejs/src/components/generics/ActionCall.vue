d6e71a4d9ac922b0028e38a92f58356f06e49675
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-31 17:23:30 +0100

    generic ActionCall (= call 2 action) component

---
+++ proj-html-vuejs/src/components/generics/ActionCall.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'ActionCall',
+    props: { label: String, title: String, paragraph: String, botton_label: String }
+}
+</script>
+
+<template>
+    <h4>{{ label }}</h4>
+    <h2>{{ title }}</h2>
+    <p>{{ paragraph }}</p>
+    <button>{{ botton_label }}</button>
+</template>
