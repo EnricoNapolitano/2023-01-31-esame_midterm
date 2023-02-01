4016faec0d2b155de8ebccdb0b032322d881fcb5
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 10:00:29 +0100

    images gallery data structure

--- proj-html-vuejs/src/assets/data/index.js
+++ proj-html-vuejs/src/assets/data/index.js
@@ -8,6 +8,14 @@ const data = {
     jumbo_paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium animi velit porro blanditiis amet ea praesentium corrupti nam consectetur aspernatur, nostrum in earum vitae, fugit repellat voluptates officia natus dignissimos.',
     jumbo_button: 'Explore our products',
 
+    //products section
+    products_label: 'OUR PRODUCTS',
+    products_title: 'All our delectable pastries are baked fresh in our Kitchen very morning, and are made with all natural, all organic, ingredients.',
+    products_button: 'Start shopping',
+
+    //products gallery
+    gallery: ['choco-chip-cookies-200x255.jpg', 'strawberry-jam-cookies-200x255.jpg', 'strawberry-donut-200x255.jpg', 'perfect-macarons-200x255.jpg'],
+
     //delivery section
     delivery_label: 'TRY OUR SEASONAL PRODUCTS',
     delivery_title: 'Order for pick-up or delivery to your home',
