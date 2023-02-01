0b0c82675572469b215733446a9e545670cd8feb
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 10:17:29 +0100

    PicturesGallery (generic): it receives a prop

---
+++ proj-html-vuejs/src/components/generics/PicturesGallery.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'PicturesGallery',
+    props: { gallery: Array }
+}
+</script>
+<template>
+    <figure>
+        <img v-for="pic in gallery" :src="pic" alt="product image">
+    </figure>
+</template>
