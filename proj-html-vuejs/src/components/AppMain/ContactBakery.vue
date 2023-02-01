583740d5932d06e7cd4be1bb9e4d1e6c4c203ee0
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 23:34:06 +0100

    ContactBakery component

---
+++ proj-html-vuejs/src/components/AppMain/ContactBakery.vue
@@ -0,0 +1,18 @@
+<script>
+import { data } from '../../assets/data';
+import SocialPictures from '../generics/SocialPictures.vue';
+export default {
+    name: 'ContactBakery',
+    data() { return { data } },
+    components: { SocialPictures }
+}
+</script>
+<template>
+    <section id="contact-bakery" class="container mv-section">
+        <social-pictures :pictures="data.social_pictures"></social-pictures>
+    </section>
+</template>
+
+<style lang="scss">
+
+</style>
