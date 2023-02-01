70c65c2afd5708d4bf3d1aac9fb4a5dcd8644aff
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 10:01:39 +0100

    ProductsSection added to AppMain

--- proj-html-vuejs/src/components/AppMain.vue
+++ proj-html-vuejs/src/components/AppMain.vue
@@ -1,10 +1,12 @@
 <script>
+import ProductsSection from './AppMain/ProductsSection.vue';
 import DeliverySection from './AppMain/DeliveySection.vue';
 export default {
     name: 'AppMain',
-    components: { DeliverySection },
+    components: { ProductsSection, DeliverySection },
 }
 </script>
 <template>
+    <products-section></products-section>
     <delivery-section></delivery-section>
 </template>
