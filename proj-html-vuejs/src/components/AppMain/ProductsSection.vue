9a03cf6022c97daab448b760ee8422139fa9dbfd
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 16:32:59 +0100

    gallery into section just as static element

--- proj-html-vuejs/src/components/AppMain/ProductsSection.vue
+++ proj-html-vuejs/src/components/AppMain/ProductsSection.vue
@@ -1,32 +1,30 @@
 <script>
 import { data } from '../../assets/data';
 import ActionCall from '../generics/ActionCall.vue';
-import ButtonsGallery from '../generics/ButtonsGallery.vue';
+import ProductsGallery from '../generics/ProductsGallery.vue';
 export default {
     name: 'ProductsSection',
     data() { return { data, currIndex: 0 } },
-    components: { ActionCall, ButtonsGallery },
-    methods: {
-        changePic(text) {
-            // last element in the gallery
-            const lastElement = this.data.gallery[this.currIndex].src.length - 1
-
-            if (text === 'prev') {
-                if (this.currIndex !== 0) this.currIndex--;
-                else this.currIndex = lastElement;
-            } else {
-                if (this.currIndex !== lastElement) this.currIndex++;
-                else this.currIndex = 0;
-            };
-        }
-    }
+    components: { ActionCall, ProductsGallery },
 }
 </script>
 <template>
-    <action-call :label="data.products_label" :title="data.products_title"
-        :button_label="data.products_button"></action-call>
-    <figure v-for="(pic, i) in data.gallery" :key="pic.id">
-        <img v-if="i === currIndex" :src="pic.src" alt="product">
-    </figure>
-    <buttons-gallery @button-clicked="changePic(text)"></buttons-gallery>
+    <section id="products-gallery" class="container d-flex jc-between ai-center mv-section">
+        <action-call :label="data.products_label" :title="data.products_title"
+            :button_label="data.products_button"></action-call>
+        <products-gallery :gallery="data.gallery.slice(0, 2)"></products-gallery>
+    </section>
 </template>
+
+<style lang="scss">
+#products-gallery {
+    figure {
+        width: 300px;
+        height: auto;
+    }
+
+    img {
+        max-width: 100%;
+    }
+}
+</style>
