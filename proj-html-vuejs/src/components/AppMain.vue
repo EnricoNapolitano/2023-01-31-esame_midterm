49740c1503d86453185211790494e975dc1efc86
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 21:41:00 +0100

    component InfoA added to AppMain

--- proj-html-vuejs/src/components/AppMain.vue
+++ proj-html-vuejs/src/components/AppMain.vue
@@ -3,14 +3,16 @@ import ProductsSection from './AppMain/ProductsSection.vue';
 import DeliverySection from './AppMain/DeliveySection.vue';
 import WeddingSection from './AppMain/WeddingSection.vue';
 import ShoppingCall from './AppMain/ShoppingCall.vue';
+import InfoA from './AppMain/InfoA.vue';
 export default {
     name: 'AppMain',
-    components: { ProductsSection, WeddingSection, ShoppingCall, DeliverySection, },
+    components: { ProductsSection, WeddingSection, ShoppingCall, InfoA, DeliverySection, },
 }
 </script>
 <template>
     <products-section></products-section>
     <wedding-section></wedding-section>
     <shopping-call></shopping-call>
+    <info-a></info-a>
     <delivery-section></delivery-section>
 </template>
