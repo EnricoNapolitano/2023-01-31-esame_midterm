5e73505edc8bfe6e9917e2711713a19849bf59c5
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 10:03:26 +0100

    ProductsSection: ActionCall in it

---
+++ proj-html-vuejs/src/components/AppMain/ProductsSection.vue
@@ -0,0 +1,13 @@
+<script>
+import { data } from '../../assets/data';
+import ActionCall from '../generics/ActionCall.vue';
+export default {
+    name: 'ProductsSection',
+    data() { return { data } },
+    components: { ActionCall },
+}
+</script>
+<template>
+    <action-call :label="data.products_label" :title="data.products_title"
+        :button_label="data.products_button"></action-call>
+</template>
