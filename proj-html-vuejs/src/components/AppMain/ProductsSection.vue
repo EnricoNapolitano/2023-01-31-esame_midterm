c703ff3974377f1ff34a6ddee48722d0cb28c674
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 10:18:28 +0100

    ProductsSection: PicturesGallery added in it

--- proj-html-vuejs/src/components/AppMain/ProductsSection.vue
+++ proj-html-vuejs/src/components/AppMain/ProductsSection.vue
@@ -1,13 +1,21 @@
 <script>
 import { data } from '../../assets/data';
 import ActionCall from '../generics/ActionCall.vue';
+import PicturesGallery from '../generics/PicturesGallery.vue';
 export default {
     name: 'ProductsSection',
     data() { return { data } },
-    components: { ActionCall },
+    components: { ActionCall, PicturesGallery },
+    computed: {
+        imagesPath() {
+            const baseUrl = '/src/assets/img/';
+            return this.data.gallery.map(img => baseUrl + img);
+        }
+    }
 }
 </script>
 <template>
     <action-call :label="data.products_label" :title="data.products_title"
         :button_label="data.products_button"></action-call>
+    <pictures-gallery :gallery="imagesPath"></pictures-gallery>
 </template>
