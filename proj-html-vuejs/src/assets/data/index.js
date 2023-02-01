cd4ed72359e88915a29b74915d9d72c5996b2256
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 16:31:23 +0100

    adding key 'title' in gallery

--- proj-html-vuejs/src/assets/data/index.js
+++ proj-html-vuejs/src/assets/data/index.js
@@ -17,30 +17,37 @@ const data = {
     gallery: [
         {
             id: '1',
+            title: 'choco chip cookies',
             src: '/src/assets/img/choco-chip-cookies-400x510.jpg',
         },
         {
             id: '2',
+            title: 'strawberry jam cookies',
             src: '/src/assets/img/strawberry-jam-cookies-400x510.jpg',
         },
         {
             id: '3',
+            title: 'strawberry donut',
             src: '/src/assets/img/strawberry-donut-400x510.jpg',
         },
         {
             id: '4',
+            title: 'perfect macarons',
             src: '/src/assets/img/perfect-macarons-400x510.jpg',
         },
         {
             id: '5',
+            title: 'cherry cake',
             src: '/src/assets/img/cherry-cake-400x510.jpg',
         },
         {
             id: '6',
+            title: 'cookies with ice cream',
             src: '/src/assets/img/cookies-with-ice-cream-400x510.jpg',
         },
         {
             id: '7',
+            title: 'blackberry stuffed bread',
             src: '/src/assets/img/blackberry-stuffed-bread-400x510.jpg',
         },
     ],
