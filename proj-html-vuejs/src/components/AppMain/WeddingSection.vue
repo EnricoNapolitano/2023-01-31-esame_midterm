06ff11d2595432aa15215dd4ec14ec4618e4678f
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 16:48:47 +0100

    created wedding section

---
+++ proj-html-vuejs/src/components/AppMain/WeddingSection.vue
@@ -0,0 +1,12 @@
+<script>
+import { data } from '../../assets/data';
+import ActionCall from '../generics/ActionCall.vue';
+export default {
+    name: 'WeddingSection',
+    data() { return { data } },
+    components: { ActionCall },
+}
+</script>
+<template>
+    <action-call :label="data.event_label" :title="data.event_title"></action-call>
+</template>
