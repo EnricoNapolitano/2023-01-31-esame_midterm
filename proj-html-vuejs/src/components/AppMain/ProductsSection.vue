d427e2e6508c21f674b8f0ebedacab661c9b8cc2
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 12:09:52 +0100

    buttons logic to change pictures (not working)

--- proj-html-vuejs/src/components/AppMain/ProductsSection.vue
+++ proj-html-vuejs/src/components/AppMain/ProductsSection.vue
@@ -1,15 +1,29 @@
 <script>
 import { data } from '../../assets/data';
 import ActionCall from '../generics/ActionCall.vue';
-import PicturesGallery from '../generics/PicturesGallery.vue';
+import ButtonsGallery from '../generics/ButtonsGallery.vue';
 export default {
     name: 'ProductsSection',
-    data() { return { data } },
-    components: { ActionCall, PicturesGallery },
+    data() { return { data, currIndex: 0 } },
+    components: { ActionCall, ButtonsGallery },
+    methods: {
+        changePic(text) {
+            if (text === 'prev') {
+                if (this.currIndex !== 0) this.currIndex--;
+                else this.currIndex = this.data.gallery.src.length - 1;
+            } else {
+                if (this.currIndex !== this.data.gallery.src.length - 1) this.currIndex++;
+                else this.currIndex = 0;
+            };
+        }
+    }
 }
 </script>
 <template>
     <action-call :label="data.products_label" :title="data.products_title"
         :button_label="data.products_button"></action-call>
-    <pictures-gallery :gallery="data.gallery"></pictures-gallery>
+    <figure v-for="(pic, i) in data.gallery">
+        <img v-if="i === currIndex" :src="pic.src" alt="product">
+    </figure>
+    <buttons-gallery @button-clicked="changePic"></buttons-gallery>
 </template>
