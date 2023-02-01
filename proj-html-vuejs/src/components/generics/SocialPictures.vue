fcd3319ac5d873bfd33fa9e009c1728bad21a2cf
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 23:34:26 +0100

    generic component SocialPictures

---
+++ proj-html-vuejs/src/components/generics/SocialPictures.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'SocialPictures',
+    props: { pictures: Array }
+}
+</script>
+<template>
+    <ul class="row d-flex jc-between">
+        <li v-for="pic in pictures"><img :src="pic" alt="social post"></li>
+    </ul>
+</template>
+
+<style lang="scss" scoped>
+.row {
+    margin: 0 -20px;
+}
+
+li {
+    width: calc(100% / 4);
+    padding: 20px;
+
+    img {
+        max-width: 100%;
+    }
+}
+</style>
