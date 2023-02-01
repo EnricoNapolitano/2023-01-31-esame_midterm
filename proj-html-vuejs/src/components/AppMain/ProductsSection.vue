8005ee2a6369b88703f5ec68fda0e37d88011a80
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 10:35:55 +0100

    change to the gallery prop

--- proj-html-vuejs/src/components/AppMain/ProductsSection.vue
+++ proj-html-vuejs/src/components/AppMain/ProductsSection.vue
@@ -6,16 +6,10 @@ export default {
     name: 'ProductsSection',
     data() { return { data } },
     components: { ActionCall, PicturesGallery },
-    computed: {
-        imagesPath() {
-            const baseUrl = '/src/assets/img/';
-            return this.data.gallery.map(img => baseUrl + img);
-        }
-    }
 }
 </script>
 <template>
     <action-call :label="data.products_label" :title="data.products_title"
         :button_label="data.products_button"></action-call>
-    <pictures-gallery :gallery="imagesPath"></pictures-gallery>
+    <pictures-gallery :gallery="data.gallery"></pictures-gallery>
 </template>
