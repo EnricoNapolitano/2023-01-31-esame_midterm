01aaeac178d274a6afc5fbf0640ebec7f391ef45
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 12:09:09 +0100

    no more pictures gallery but buttons gallery

---
+++ proj-html-vuejs/src/components/generics/ButtonsGallery.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+    name: 'ButtonsGallery',
+    emits: ['button-clicked']
+}
+</script>
+<template>
+    <button class="next" @click="$emit('button-clicked', 'next')">next</button>
+    <button class="prev" @click="$emit('button-clicked', 'prev')">prev</button>
+</template>
