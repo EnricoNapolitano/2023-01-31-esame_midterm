cc3843fffc6e84a8bd3a1c5aeb64019bc9072308
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 16:49:08 +0100

    added wedding section to AppMain

--- proj-html-vuejs/src/components/AppMain.vue
+++ proj-html-vuejs/src/components/AppMain.vue
@@ -1,12 +1,14 @@
 <script>
 import ProductsSection from './AppMain/ProductsSection.vue';
 import DeliverySection from './AppMain/DeliveySection.vue';
+import WeddingSection from './AppMain/WeddingSection.vue'
 export default {
     name: 'AppMain',
-    components: { ProductsSection, DeliverySection },
+    components: { ProductsSection, DeliverySection, WeddingSection },
 }
 </script>
 <template>
     <products-section></products-section>
+    <wedding-section></wedding-section>
     <delivery-section></delivery-section>
 </template>
