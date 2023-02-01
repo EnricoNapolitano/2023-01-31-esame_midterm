f9566e0b88304312329e6aea4973ca875cc3dd02
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-01 16:30:48 +0100

    component  ProductsGallery and not ButtonGallery

---
+++ proj-html-vuejs/src/components/generics/ProductsGallery.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'ProductsGallery',
+    props: { gallery: Array },
+}
+</script>
+<template>
+    <figure>
+        <img v-for="item in gallery" :src="item.src" :alt="item.title">
+    </figure>
+</template>
+
+<style scoped>
+figure {
+    display: flex;
+    justify-content: flex-end;
+}
+</style>
