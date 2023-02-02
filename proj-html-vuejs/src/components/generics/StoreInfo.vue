904e06fee43740fa8266a19b992979c5b3277f6c
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-02 01:22:27 +0100

    StoreInfo component

---
+++ proj-html-vuejs/src/components/generics/StoreInfo.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'StoreInfo',
+    props: { city: String }
+}
+</script>
+<template>
+    <h2>{{ city }}</h2>
+    <div class="row d-flex jc-around ai-center">
+        <div>
+            <h4>CALL US</h4>
+            <div>1.800.458.556</div>
+        </div>
+        <div>
+            <h4>OPEN ALL WEEK</h4>
+            <div>9.00 AM - 6.00 PM</div>
+        </div>
+        <button>View Map</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+h2 {
+    text-align: center;
+    font-size: 0.8rem;
+    margin-bottom: 30px;
+}
+
+h4 {
+    padding-bottom: 18px;
+}
+
+button {
+    padding: 15px 20px;
+}
+</style>
