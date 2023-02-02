1f42c72462accc0f6d5b57a7f60d7d1d4ae96f48
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-02 10:28:40 +0100

    style of input area

--- proj-html-vuejs/src/components/generics/InputForm.vue
+++ proj-html-vuejs/src/components/generics/InputForm.vue
@@ -8,19 +8,30 @@ export default {
 <template>
     <form>
         <input type="text" v-model.trim="value" :placeholder="placeholder || 'Type here...'">
-        <button> {{ text || 'SEND' }}</button>
+        <button class="btn"> {{ text || 'SEND' }}</button>
     </form>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use '../../assets/scss/partials/variables' as*;
+
 input {
     width: 350px;
     height: 40px;
     padding-left: 20px;
+
+    border: 1px solid $violet3;
+    border-radius: 5px;
+
+    &:focus {
+        outline: none;
+        border-color: $violet4;
+    }
 }
 
 button {
     height: 40px;
     padding: 0 20px;
+    margin-left: 10px;
 }
 </style>
